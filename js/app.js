e0c5d73f337a6a78a2e31f290a668d2fedb84dd4
Aaron Dudue <duduzilesibanda99@gmail.com>
2021-10-01 16:17:57 +0200

    animations

---
+++ js/app.js
@@ -0,0 +1,3 @@
+gsap.from('nav', {opacity: 0, duration: 1.5, y:-50, ease: Back.easeInOut})
+gsap.from('.hero-text', {duration: 1.5, opacity: 0, x: -50, ease: Back.easeInOut})
+gsap.from('.hero-image', {duration: 1.5, opacity: 0, x: 50, ease: Back.easeInOut})
